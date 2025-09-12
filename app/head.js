6520880601e6c52f65548ccdfd270cbb4416aba0
moha-barre <mohab4rre1@gmail.com>
2025-09-12 15:15:31 +0300

    updated google console i:2

---
+++ app/head.js
@@ -0,0 +1,27 @@
+
+export default function HeadComponent() {
+  return (
+    <>
+      <title>Somhay - Global products, Local acces</title>
+      <meta
+        name="google-site-verification"
+        content="WdnT6pwogkSSvv8-uE3E8YpEUcuiwg1I3CH-I9dC3qQ"
+      />
+      <meta
+        name="description"
+        content="Somhay helps you source products from marketplaces like 1688 and Alibaba quickly and reliably. Get high-quality products delivered to your doorstep."
+      />
+      <meta
+        name="keywords"
+        content="1688 sourcing, Alibaba products, global product sourcing, Somhay delivery, order products online"
+      />
+      <meta property="og:title" content="Somhay - Global products, Local acces" />
+      <meta property="og:image" content="/hero.jpg" />
+      <meta property="og:url" content="https://somhay.vercel.app" />
+      <meta
+        property="og:description"
+        content="Somhay helps you source products from marketplaces like 1688 and Alibaba quickly and reliably."
+      />
+    </>
+  );
+}
