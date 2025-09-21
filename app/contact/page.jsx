15256ad81fedef530328d04aea05c7f68891e6f7
moha-barre <mobarre001@gmail.com>
2025-09-21 20:27:59 +0300

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import SEO from "@/components/SEO"
 import Process from "@/components/Process";
-import Faq from "@/components/Faq;
+import Faq from "@/components/Faq";
 
 import { Mail, Phone, Instagram, Facebook, Linkedin, MessageCircle } from "lucide-react";
 
